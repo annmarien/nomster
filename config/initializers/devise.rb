4901c5c51d63fff745a462e6b21c38e3faf152a1
AnnMarie Norcross <amnorcross@charter.net>
2014-04-30 19:48:23 +0000

    Lesson 19 style the sign in page

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,7 +5,9 @@ Devise.setup do |config|
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
   # config.secret_key = '9fb507d3f970bccbba6b78d3c5d3064676f37ef7ed75eae23839fe6ef547b6fd3590f2678a22c2402bf6eb7799b8d2260df242875de58af7200700a16129aaff'
-
+  if Rails.env.production?
+     config.secret_key = ENV['DEVISE_SECRET']
+  end
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
